7296cd478c7944feecbd993c238eb0bfa4c9e1bd
CodeMayank-01 <mayankg19112003@gmail.com>
2024-07-01 21:09:35 +0530

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,93 @@
+window.addEventListener('load',bindEvents);
+var buttons;
+function bindEvents(){
+buttons = document.getElementsByTagName('button');
+console.log(buttons);
+for(var i=0; i<buttons.length; i++){
+    var currentButton = buttons[i];
+    currentButton.addEventListener('click',printXorZero); //event binding
+}
+}
+
+function isNotBlank(currentButton){
+    return currentButton.innerText.trim().length>0
+}
+
+function isThreeSame(first,second,third){
+    if(isNotBlank(first) && isNotBlank(second) && isNotBlank(third)){
+    return first.innerText==second.innerText && first.innerText==third.innerText;
+    } 
+    else{
+        return false;
+    }
+}
+// maintain a counter , to check if counter goes
+// >4 then check game over condition 
+function isGameOver(){
+    return isThreeSame(buttons[0], buttons[1], buttons[2])
+    || isThreeSame(buttons[3], buttons[4], buttons[5])
+    || isThreeSame(buttons[6], buttons[7], buttons[8])
+    || isThreeSame(buttons[0], buttons[3], buttons[6])
+    || isThreeSame(buttons[1], buttons[4], buttons[7])
+    || isThreeSame(buttons[2], buttons[5], buttons[8])
+    || isThreeSame(buttons[0], buttons[4], buttons[8])
+    || isThreeSame(buttons[2], buttons[4], buttons[6])
+
+    // is game over condition 
+    //1. row values are same 
+    // 2. cols values are same
+    // 3. diagonal values are same 
+ 
+}
+
+function reset(){
+    clearInterval(interval);
+    blocked = false;
+    count = 0;
+    flag = true;
+    countDownValue = 5;
+    for(var i=0; i<buttons.length; i++){
+        buttons[i].innerText='';
+    }
+    document.getElementById('output').innerText =  '';
+}
+var countDownValue = 5;
+var interval;
+function countDown(){
+    interval = setInterval(function(){
+        countDownValue--;
+        document.getElementById('output').innerText = `Game is over & Reset After ${countDownValue}Sec`;
+    },1000);
+}
+function resetAfter5Sec(){
+    countDown();
+    // open a new thread
+    setTimeout(function(){
+        reset();
+    }, 5000);
+    console.log('Doing Something else');
+}
+// location.reload;
+    //reset the page 
+var flag = true;
+var count = 0;
+var blocked = false;
+function printXorZero(){
+    if(!blocked && this.innerText.trim().length==0){
+        count++;
+       
+    var buttonValue =flag?"X":"0";
+    this.innerText= buttonValue; // <button><button>
+    flag = !flag;
+    if(count>=5){
+            if(isGameOver()){
+                blocked = true;
+                document.getElementById('output').innerText = `Game is over & Reset After 5 Sec`;
+                resetAfter5Sec();
+                // alert("game over")
+            }
+    }
+    }
+    //alert("u click the button")
+ }
+ 
